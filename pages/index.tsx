e2b3e1475b892fc90cc3f527d776b33d29952c51
spidΞy <web3slinger@gmail.com>
2023-02-01 21:14:33 -0800

    fix: add git lfs

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
           <p>class: one handed</p>
           <p>
             download:{' '}
-            <a className="text-indigo-500" href="/magikwand-0.1.0-arm64-mac.zip" download>
+            <a className="text-indigo-500" href="/magikwand.dmg" download>
               macOS
             </a>
           </p>
